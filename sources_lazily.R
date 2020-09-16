61e3d771cf5d4b675321bc8a71a0ee1a6eaf2b8a
m.2 <secure00@gmail.com>
2020-09-16 16:04:30 +0200

    Adding sources_lazily Removing data folder

---
+++ sources_lazily.R
@@ -0,0 +1,18 @@
+## header ##
+## who:    m.2
+## when:   Wed Sep 16 16:02:43 2020
+## why:    Sources the script lazily only if not sourced.
+############
+
+sources_lazily <- function(file_name){
+  if (is.null(.GlobalEnv$m.2_sourced)) {
+    .GlobalEnv$m.2_sourced <- c(file_name)
+  }else{
+    sourced_file_name <-
+      .GlobalEnv$m.2_sourced[.GlobalEnv$m.2_sourced == file_name]
+    length_sourced_file <- length(sourced_file_name)
+    if (length_sourced_file <= 0) {
+      .GlobalEnv$m.2_sourced <- rbind(.GlobalEnv$m.2_sourced, file_name)
+    }
+  }
+}
